09272b2eb93b25c16a5e9c9e839493fd54665d25
ClaudioFelippeNovaisBarboza <49801577+ClaudioFelippeNovaisBarboza@users.noreply.github.com>
2021-06-19 16:01:45 -0300

    teste 01

--- Data/Produto.cs
+++ Data/Produto.cs
@@ -12,5 +12,7 @@ namespace API_Crud_Clientes.Data
         public decimal Preco { get; set; }
         public int Estoque { get; set; }
         public int Estoque01 { get; set; }
+        public int Estoque02 { get; set; }
+
     }
 }
