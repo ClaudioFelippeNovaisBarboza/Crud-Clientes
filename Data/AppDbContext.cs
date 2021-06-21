241dfcbf648f2cdb362d8f8034a76ca9d94a6c2a
ClaudioFelippeNovaisBarboza <49801577+ClaudioFelippeNovaisBarboza@users.noreply.github.com>
2021-06-21 16:45:58 -0300

    Alteração de Produto para Cliente

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -13,25 +13,61 @@ namespace API_Crud_Clientes.Data
         
         }
 
-        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Produto>()
-                .Property(p => p.Descricao)
-                    .HasMaxLength(80);
+            modelBuilder.Entity<Cliente>()
+                .Property(p => p.nome)
+                    .HasMaxLength(50);
 
-            modelBuilder.Entity<Produto>()
-                .Property(p => p.Preco)
-                    .HasPrecision(10, 2);
+            modelBuilder.Entity<Cliente>()
+                .Property(p => p.nacionalidade)
+                    .HasMaxLength(20);
 
-            modelBuilder.Entity<Produto>()
+            modelBuilder.Entity<Cliente>()
                 .HasData(
-                    new Produto { Id = 1, Descricao = "Caderno", Estoque = 4, Preco = 7.12M },
-                    new Produto { Id = 2, Descricao = "Lapis", Estoque = 4, Preco = 7.12M },
-                    new Produto { Id = 3, Descricao = "Estojo", Estoque = 4, Preco = 7.12M },
-                    new Produto { Id = 4, Descricao = "Borracha", Estoque = 4, Preco = 7.12M },
-                    new Produto { Id = 5, Descricao = "Apontador", Estoque = 4, Preco = 7.12M }
+                    new Cliente
+                    {
+                        Id = 1,
+                        nome = "Joao Silva",
+                        rg = 521854225,
+                        cpf = 88554522274,
+                        sexo = "M",
+                        nascimento = DateTime.Now,
+                        nacionalidade = "Brasileiro",
+                        altura = 00175,
+                        dataCadastro = DateTime.Now,
+                        ativo = "S"
+                    },
+
+                    new Cliente
+                    {
+                        Id = 1,
+                        nome = "Aline Dias",
+                        rg = 845625874,
+                        cpf = 37409703830,
+                        sexo = "M",
+                        nascimento = DateTime.Now,
+                        nacionalidade = "Brasileiro",
+                        altura = 00175,
+                        dataCadastro = DateTime.Now,
+                        ativo = "S"
+                    },
+
+                    new Cliente
+                    {
+                        Id = 1,
+                        nome = "Marcio Souza",
+                        rg = 416499089,
+                        cpf = 33355577741,
+                        sexo = "M",
+                        nascimento = DateTime.Now,
+                        nacionalidade = "Brasileiro",
+                        altura = 00175,
+                        dataCadastro = DateTime.Now,
+                        ativo = "S"
+                    }
                 );
         }
     }
---
+++ Data/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Crud_Clientes.Data
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int rg { get; set; }
+        public long cpf { get; set; }
+        public string sexo { get; set; }
+        public DateTime nascimento { get; set; }
+        public string nacionalidade { get; set; }
+        public decimal altura { get; set; }
+        public DateTime dataCadastro { get; set; }
+        public string ativo { get; set; }
+    }
+}
